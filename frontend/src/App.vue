232e7e592f1dc1f887c281ef553c7d860dc6c738
robyzzz <robyawyx@gmail.com>
2022-07-26 16:58:23 +0100

    added initial frontend setup
    contains vue3, vite, typescript and vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="increment">
+    <h1> Count is: {{ count }} </h1>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
+
